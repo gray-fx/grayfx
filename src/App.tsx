6cfa8da466c1eb5bc16c05eebdbfc26af9b53f82
grayfx <129699207+gray-fx@users.noreply.github.com>
2026-03-08 20:26:56 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Status from "./pages/Status";
+import StatusUpdate from "./pages/StatusUpdate";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/status" element={<Status />} />
+          <Route path="/statusupdate" element={<StatusUpdate />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
