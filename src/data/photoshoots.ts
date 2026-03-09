eed09806c8af20344ce9d8ce40f8be16452a18f4
grayfx <129699207+gray-fx@users.noreply.github.com>
2026-03-08 21:04:58 -0400

    Update photoshoots.ts

--- src/data/photoshoots.ts
+++ src/data/photoshoots.ts
@@ -1,4 +1,8 @@
-export type ShootStatus = "Not Shot" | "Editing" | "Completed";
+// =============================================
+// Edit this file to update photoshoot statuses!
+// =============================================
+
+export type ShootStatus = "Not Shot" | "Awaiting Edits" | "Editing" | "Completed";
 
 export interface Photoshoot {
   name: string;
@@ -7,34 +11,12 @@ export interface Photoshoot {
   link?: string;
 }
 
-export const photoshoots: Photoshoot[] = [
-  {
-    name: "Delaware Blue Coats",
-    status: "Not Shot",
-    date: "Mar 12, 2026"
-  },
-  {
-    name: "DEFC Soccer",
-    status: "Completed",
-    date: "Feb 28, 2026",
-    link: "https://photos.grayfx.cam/defcsoccer/"
-  },
-  {
-    name: "DIAA Swimming Finals",
-    status: "Completed",
-    date: "Feb 27, 2026",
-    link: "https://photos.grayfx.cam/saintmarksswimming/"
-  },
-  {
-    name: "SJB Intermediate Gray",
-    status: "Completed",
-    date: "Feb 26, 2026",
-    link: "https://photos.grayfx.cam/sjbhoopsgraysmm/227atsmos/"
-  },
-  {
-    name: "SJB Varsity Y",
-    status: "Completed",
-    date: "Feb 16, 2026",
-    link: "https://photos.grayfx.cam/stjohnsy/"
-  }
+const photoshoots: Photoshoot[] = [
+  { name: "Delaware Blue Coats", status: "Not Shot", date: "Mar 13, 2026" },
+  { name: "DEFC Soccer", status: "Completed", date: "Mar 1, 2026", link: "https://photos.grayfx.cam/defcsoccer/" },
+  { name: "DIAA Swimming Finals", status: "Completed", date: "Feb 28, 2026", link: "https://photos.grayfx.cam/saintmarksswimming/" },
+  { name: "SJB Intermediate Gray", status: "Completed", date: "Feb 27, 2026", link: "https://photos.grayfx.cam/sjbhoopsgraysmm/227atsmos/" },
+  { name: "SJB Varsity Y", status: "Completed", date: "Feb 17, 2026", link: "https://photos.grayfx.cam/stjohnsy/" },
 ];
+
+export default photoshoots;
