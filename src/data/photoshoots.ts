af4f76e900d59964331e4feb86b9b1e094e65215
grayfx <129699207+gray-fx@users.noreply.github.com>
2026-03-09 22:32:45 -0400

    Update photoshoots.ts

--- src/data/photoshoots.ts
+++ src/data/photoshoots.ts
@@ -9,43 +9,47 @@ export interface Photoshoot {
 
 export const photoshoots: Photoshoot[] = [
   {
+    name: "",
+    status: "Upcoming"
+  },
+  {
     name: "Saint Mark's Baseball",
-    status: "Upcoming",
-    date: "Mar 13, 2026"
+    status: "Awaiting Edits",
+    date: "Mar 12, 2026"
   },
   {
     name: "Delaware Blue Coats",
-    status: "Upcoming",
-    date: "Mar 12, 2026"
+    status: "Editing",
+    date: "Mar 11, 2026"
   },
   {
     name: "DEFC Soccer",
     status: "Completed",
-    date: "Feb 26, 2026",
+    date: "Feb 25, 2026",
     link: "https://photos.grayfx.cam/defcsoccer/"
   },
   {
     name: "DIAA Swimming Finals",
     status: "Completed",
-    date: "Feb 25, 2026",
+    date: "Feb 24, 2026",
     link: "https://photos.grayfx.cam/saintmarksswimming/"
   },
   {
     name: "SJB Intermediate Gray",
     status: "Completed",
-    date: "Feb 24, 2026",
+    date: "Feb 23, 2026",
     link: "https://photos.grayfx.cam/sjbhoopsgraysmm/227atsmos/"
   },
   {
     name: "SJB Varsity Y",
     status: "Completed",
-    date: "Feb 14, 2026",
+    date: "Feb 13, 2026",
     link: "https://photos.grayfx.cam/stjohnsy/"
   },
   {
     name: "Saint Mark's Basketball",
     status: "Completed",
-    date: "Feb 14, 2026",
+    date: "Feb 13, 2026",
     link: "https://photos.grayfx.cam/saintmarksvsstesbball/"
   }
 ];
