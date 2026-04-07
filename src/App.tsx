89dae88612974c3638bbeab688c70fd9d102e702
grayfx <129699207+gray-fx@users.noreply.github.com>
2026-04-07 11:24:37 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
 import Availability from "./pages/Availability";
-import AdminAvailability from "./pages/AdminPanel";
+import AdminPanel from "./pages/AdminPanel";
 import UnfollowFinder from "./pages/UnfollowFinder";
 import ScoreboardControl from "./pages/ScoreboardControl";
 import ScoreboardDisplay from "./pages/ScoreboardDisplay";
@@ -25,7 +25,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/availability" element={<Availability />} />
-          <Route path="/admin/availability" element={<AdminAvailability />} />
+          <Route path="/admin/panel" element={<AdminPanel />} />
           <Route path="/unfollow" element={<UnfollowFinder />} />
           <Route path="/scoreboard-control" element={<ScoreboardControl />} />
           <Route path="/scoreboard" element={<ScoreboardDisplay />} />
